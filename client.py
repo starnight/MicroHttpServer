233bf07322fe9d71cebf8e09b3656242ceb01308
StarNight <starnight@g.ncu.edu.tw>
2015-12-11 15:41:01 +0000

    Use client-server test scenario.

---
+++ client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import unittest
+import http.client
+
+url = "localhost:8000"
+
+class Client:
+	def test_Connect(self):
+		connected = 0
+		try:
+			self.conn = http.client.HTTPConnection(url)
+			self.conn.connect()
+			connected = 1
+		except Exception:
+			print(Exception)
+
+		return connected
+			
+	def test_Close(self):
+		self.conn.close()
+		return 1
+
+class TestServer(unittest.TestCase):
+	def test_Scenario1(self):
+		cli = Client()
+		self.assertEqual(cli.test_Connect(), 1)
+		self.assertEqual(cli.test_Close(), 1)
+
+if __name__ == "__main__":
+	unittest.main()
